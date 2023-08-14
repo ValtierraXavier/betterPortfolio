a9282ec247d3de210fc8d19fc9b9e1f887e47ce7
ValtierraXavier <xavier.valtierra@icloud.com>
2023-08-14 12:24:25 -0400

    routine commit.

--- src/Components/ContactTab/ContactTab.jsx
+++ src/Components/ContactTab/ContactTab.jsx
@@ -1,37 +1,47 @@
 import React, {useEffect, useState} from 'react'
 import './ContactTab.css'
-import { render } from '@testing-library/react'
 
 
 export default function ContactTab() {
-    const[emailBody, setEmailBody] = useState('')
     const[mailTo, setMailTo] = useState('mailto:xavier.valtierra@icloud.com')
-    let urlEncodedBody
+    const[emailBody, setEmailBody] = useState('')
     const[emailSubject, setEmailSubject] = useState('')
-    console.log(mailTo)
+    const[buttonToggle, setButtonToggle] = useState('Contact Me')
     
     const handleUrlEncode =()=>{
-        const bodyArr = [...emailBody]
-        const halfEncoded = bodyArr.toString().replace(/ /g, '%20')
-        urlEncodedBody = halfEncoded.replace(/,/g, '')
-        setMailTo(prev =>prev = `mailto:xavier.valtierra@icloud.com?subject=${emailSubject}&body=${urlEncodedBody}`)
+        const urlEncodedBody = emailBody.replaceAll(' ', '%20')
+        setMailTo(prev=>prev = `mailto:xavier.valtierra@icloud.com?subject=${emailSubject}&body=${urlEncodedBody}`)
+        handleClearInputs()
     };
 
+    const handleClearInputs =()=>{
+        setEmailSubject(prev => prev = '')
+        setEmailBody(prev => prev = '')
+    }
     
+    const toggleButtonText = ()=>{
+        if(buttonToggle === 'Contact Me'){
+            setButtonToggle(prev=>prev='Close')
+        } else if(buttonToggle === 'Close') {
+            setButtonToggle(prev=>prev='Contact Me')
+        }
+    }
     
     
   return (
     
     <div className = 'contactTabContainer'>
-    
-        <div className = 'contactButton' id = 'contactDiv'>Contact Me</div>    
+        
+        {/* <div className = 'contactButton' id = 'contactButton'>Contact Me</div>   */}
+        <label className = 'contactButton' id = "contactButton" for='toggleContact' onClick={toggleButtonText}>{buttonToggle}</label>  
+        <input type='checkbox' id = 'toggleContact'></input>
         <div className = 'contactButton' id = 'formDiv'>
             <h3 className = 'contactFormTitle'>Send me an Email</h3>
             <form action={mailTo}  method = 'get' encType='plain/text' className = 'contactForm'>
                 <p id = 'subjectFormLabel' className = 'emailFormLabels'>Subject</p>
-                <input name='subject' onChange={(e)=> setEmailSubject(prev => prev = e.target.value)} className = 'emailInput' id = 'subjectInput' type='text'></input>
+                <input name='subject' onChange={(e)=> setEmailSubject(prev => prev = e.target.value)} value ={emailSubject} className = 'emailInput' id = 'subjectInput' type='text'></input>
                 <p id = 'bodyFormLabel' className = 'emailFormLabels'>Text</p>
-                <textarea name ='body' onChange={(e)=> setEmailBody(prev => prev = e.target.value)} className = 'emailInput' id = 'bodyInput' type='text'></textarea>
+                <textarea name ='body' onChange={(e)=> setEmailBody(prev => prev = e.target.value)} value ={emailBody} className = 'emailInput' id = 'bodyInput' type='text'></textarea>
                 <a className='mailToLink' onClick={handleUrlEncode} href = {`${mailTo}`} >Send Email!</a>
             </form>
         </div>
